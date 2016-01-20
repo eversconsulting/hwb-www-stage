5682f00edbd73603d4faf4c1848470db55c6ac47
Brandon Evers <bevers222@gmail.com>
2016-01-20 12:24:36 -0600

    Made sliding tabs for mobile, added font

--- script.js
+++ script.js
@@ -1,13 +1,108 @@
 $(document).ready(function(){
-   $(".section-arrow").click(function() {
-   		if($(this).css("background-color")=="#0000")
-   		{
-   			$(this).css("background-color","blue");
-   		}
-   		else
-   		{
-   			$(this).css("background-color","black");
-   		}
+	$sec1Clicked = false;
+	$sec2Clicked = false;
+	$sec3Clicked = false;
+   $("#sec1").click(function() {
    		
+   		if($sec1Clicked == false){
+   			$(this).animate({
+				    height: "400"
+				  }, 500, function() {
+				    // Animation complete.
+				});
+   			$sec1Clicked = true;
+
+   			$("#sec2").animate({
+				    height: "75"
+				  }, 500, function() {
+				    // Animation complete.
+				});
+   			$sec2Clicked = false;
+    		$("#sec3").animate({
+				    height: "75"
+				  }, 500, function() {
+				    // Animation complete.
+				});
+   			$sec3Clicked = false;
+   		}
+   		else{
+   			$(this).animate({
+				    height: "75"
+				  }, 500, function() {
+				    // Animation complete.
+				});
+
+   			$sec1Clicked = false;
+
+
+   		}
+	});
+
+   $("#sec2").click(function() {
+   		
+   		if($sec2Clicked == false){
+   			$(this).animate({
+				    height: "400"
+				  }, 500, function() {
+				    // Animation complete.
+				});
+   			$sec2Clicked = true;
+
+   			$("#sec1").animate({
+				    height: "75"
+				  }, 500, function() {
+				    // Animation complete.
+				});
+   			$sec1Clicked = false;
+    		$("#sec3").animate({
+				    height: "75"
+				  }, 500, function() {
+				    // Animation complete.
+				});
+   			$sec3Clicked = false;
+   		}
+   		else{
+   			$(this).animate({
+				    height: "75"
+				  }, 500, function() {
+				    // Animation complete.
+				});
+
+   			$sec2Clicked = false;
+   		}
+	});
+
+   $("#sec3").click(function() {
+   		
+   		if($sec3Clicked == false){
+   			$(this).animate({
+				    height: "400"
+				  }, 500, function() {
+				    // Animation complete.
+				});
+   			$sec3Clicked = true;
+
+   			$("#sec1").animate({
+				    height: "75"
+				  }, 500, function() {
+				    // Animation complete.
+				});
+   			$sec1Clicked = false;
+    		$("#sec2").animate({
+				    height: "75"
+				  }, 500, function() {
+				    // Animation complete.
+				});
+   			$sec2Clicked = false;
+   		}
+   		else{
+   			$(this).animate({
+				    height: "75"
+				  }, 500, function() {
+				    // Animation complete.
+				});
+
+   			$sec3Clicked = false;
+   		}
 	});
 }); 
