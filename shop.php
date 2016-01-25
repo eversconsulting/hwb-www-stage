df1c8e42440ad24f8277d87ab06ad0499e89ff53
Brandon Evers <bevers222@gmail.com>
2016-01-25 13:14:49 -0600

    Prototype for shop page

---
+++ shop.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html> 
+<head>
+	<title>Homewood Bat</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	<script type="text/javascript" src="jquery.js"></script>
+	<script type="text/javascript" src="script.js"></script>
+</head>
+<body>
+	<div class="navbar">
+		<div class="logo"></div>
+		
+		<ul class="navbar-items">
+			<li>Home</li>
+			<li>Shop</li>
+			<li>About</li>
+		</ul>
+		<div class="hamburger"></div>
+	</div>
+	<div class="navbar-spacer"></div>
+	<div class="slide-menu">
+
+	</div>
+
+	
+	<div class="section" id="sec1" style="background-color: #2F4F4F;">
+		<div class="spacer"></div>
+		<div class="shop-item-container">
+
+			<div class="shop-image"></div>
+			<div class="select-container">
+				<select>
+					<option>Blue</option>
+					<option>Red</option>
+					<option>Green</option>
+				</select>
+			</div>
+
+
+
+		</div>
+
+	</div>
+</body>
+</html>
