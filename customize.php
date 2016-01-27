19b059b81815210c01e869ffea639faf48cfd285
Brandon Evers <bevers222@gmail.com>
2016-01-27 12:15:47 -0600

    changed to customize, added for all sizes

--- customize.php
+++ customize.php
@@ -29,6 +29,11 @@
 		<div class="shop-item-container">
 
 			<div class="shop-image"></div>
+			<div class="shop-desc"> loream ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			</div>
 			<div class="select-container">
 				<select>
 					<option>Blue</option>
