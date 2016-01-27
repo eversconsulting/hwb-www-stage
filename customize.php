054fcf7df2cee02674c2ef6d23464116ea1f822b
Brandon Evers <bevers222@gmail.com>
2016-01-27 12:30:16 -0600

    Desktop Layout for customize

--- customize.php
+++ customize.php
@@ -29,11 +29,28 @@
 		<div class="shop-item-container">
 
 			<div class="shop-image"></div>
+			<div class="color-picker">
+				<ul class="colors">
+					<li class="color">red</li>
+					<li class="color">blue</li>
+					<li class="color">green</li>
+				</ul>
+
+			</div>
+			<div class="color-picker">
+				<ul class="colors">
+					<li class="color">red</li>
+					<li class="color">blue</li>
+					<li class="color">green</li>
+				</ul>
+
+			</div>
 			<div class="shop-desc"> loream ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 			</div>
+
 			<div class="select-container">
 				<select>
 					<option>Blue</option>
@@ -41,6 +58,7 @@
 					<option>Green</option>
 				</select>
 			</div>
+			
 
 
 
