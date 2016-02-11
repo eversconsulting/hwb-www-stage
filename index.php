2dbbb7ab72a9af38a1915e24d0afaec4ccf3bda3
Brandon Evers <bevers222@gmail.com>
2016-02-11 05:16:28 -0600

    Mobile view for index.php

--- index.php
+++ index.php
@@ -13,23 +13,38 @@
 		<div class="logo"></div>
 		
 		<ul class="navbar-items">
-			<a href="index.php"><li>HOME</li></a>
-			<li>SHOP</li>
-			<li>NEWS</li>
-			<li>CONTACT</li>
-			<li>ABOUT</li>
+			<li id="home">HOME</li>
+			<li id="shop">SHOP</li>
+			<li id="news">NEWS</li>
+			<li id="contact">CONTACT</li>
+			<li id="about">ABOUT</li>
 			<a href="cart.php"><li id="cartShow">CART ( )</li></a>
 		</ul>
 
-		<div class="navbar-icons">
+		<!-- <div class="navbar-icons">
 			<div class="nav-icon"></div>
 			<div class="nav-icon"></div>
 			<div class="nav-icon"></div>
-		</div>
-		<div class="hamburger"></div>
+		</div> -->
+
+		<div class="hamburger">
+			<div id="ham1" class="hamburger-line"></div>
+			<div id="ham2" class="hamburger-line"></div>
+			<div id="ham3" class="hamburger-line"></div>
+		</div>		
 	</div>
 	<div class="navbar-spacer"></div>
 	<div class="slide-menu">
+		<div id="mhome" class="slide-option">HOME</div>
+		<div id="mshop" class="slide-option">SHOP</div>
+		<div id="mnews" class="slide-option">NEWS</div>
+		<div id="mcontact" class="slide-option">CONTACT</div>
+		<div id="mabout" class="slide-option">ABOUT</div>
+		<div class="slide-option">CART ( )</div>
+
+
+	</div>
+	
 
 	<script type="text/javascript">
 		$cart = sessionStorage.getItem("cart");
@@ -58,7 +73,7 @@
 				<div class="section-title">SHOP</div>
 				<div class="title-line"></div>
 			</div>
-				<div class="shop-inner">
+				<div id="desktop-shop" class="shop-inner">
 					<div class="shop-container" id="s1">
 						<div class="shop-item">
 							<a href="customize.php?id=pro"><div class="item-picture"></div></a>
@@ -98,7 +113,38 @@
 						
 						
 					</div>
-				</div>		
+				</div>
+
+				<div id="mobile-shop" class="mobile-shop">
+					<a href="customize.php?id=pro"><div class="mobile-item">
+						<div class="mobile-item-title">PRO BAT</div>
+						<div class="mobile-item-selector">&#x2771;</div>
+					</div></a>
+					<a href="customize.php?id=game"><div class="mobile-item">
+						<div class="mobile-item-title">GAME BAT</div>
+						<div class="mobile-item-selector">&#x2771;</div>
+					</div></a>
+					<a href="customize.php?id=youth"><div class="mobile-item">
+						<div class="mobile-item-title">YOUTH BAT</div>
+						<div class="mobile-item-selector">&#x2771;</div>
+					</div></a>
+					<a href="customize.php?id=fungo"><div class="mobile-item">
+						<div class="mobile-item-title">FUNGO BAT</div>
+						<div class="mobile-item-selector">&#x2771;</div>
+					</div></a>
+					<a href="customize.php?id=promo"><div class="mobile-item">
+						<div class="mobile-item-title">PROMOTIONAL BAT</div>
+						<div class="mobile-item-selector">&#x2771;</div>
+					</div></a>
+					<div class="mobile-item">
+						<div class="mobile-item-title">ACCESSORIES</div>
+						<div class="mobile-item-selector">&#x2771;</div>
+					</div>
+					<div id="gc" class="mobile-item">
+						<div class="mobile-item-title">GIFT CARD</div>
+						<div class="mobile-item-selector">&#x2771;</div>
+					</div>
+				</div>	
 	</div>
 	<div class="section" id="sec3" style="background-color: rgb(250, 245, 234);">
 		<div class="title-row">
@@ -111,14 +157,13 @@
 			
 		</div>
 	</div>
-	<div class="section" id="sec4" style="background-color: rgb(250, 245, 234); height: 600px;">
+	<div class="section" id="sec4" style="background-color: rgb(250, 245, 234);">
 		<div class="title-row">
 				<div class="title-line"></div>
 				<div class="section-title">CONTACT</div>
 				<div class="title-line"></div>
 			</div>
-		<div class="section-inner" style="height: auto;">
-			
+		<div id="desktop-contact" class="section-inner" style="height: auto;">
 				<div class="cart-col" style="width: auto;">
 					<div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2985.1704947532794!2d-87.63521178505906!3d41.56555449289474!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880e18bb669d2c65%3A0xff83244a990b75e2!2s17845+Bretz+Dr%2C+Homewood%2C+IL+60430!5e0!3m2!1sen!2sus!4v1455171054605" width="400" height="400" frameborder="0" style="border:0" allowfullscreen></iframe></div>
 				</div>
@@ -133,11 +178,20 @@
 							<p>
 							HOMEWOOD BAT CO<br>17845 BRETS DRIVE<br>HOMEWOOD, ILLINOIS 60430
 						</div>
-
-
 					</div>
 				</div>
-			
+		</div>
+
+		<div id="mobile-contact" class="mobile-contact">
+			<div class="contact-info">
+				<div class="contact-p">
+					<div>We'd love to talk with you about your special bat needs. Please feel free to give us a call, send an email, or stop in for a look at our shop.</div>
+				</div>
+				<p>
+				708.713.8000<br>INFO@HOMEWOODBAT.COM
+				<p>
+				HOMEWOOD BAT CO<br>17845 BRETS DRIVE<br>HOMEWOOD, ILLINOIS 60430
+			</div>
 		</div>
 	</div>
 	<div class="section" id="sec5" style="background-color: rgb(250, 245, 234);">
@@ -146,7 +200,27 @@
 				<div class="section-title">ABOUT</div>
 				<div class="title-line"></div>
 			</div>
-		<div class="section-inner">
+		<div id="desktop-about" class="section-inner">
+			<div class="about-title">WHY ANOTHER BAT COMPANY?</div>
+			<p>
+			<div class="about-body">
+				<div class="about-sub">Because this is a baseball town.<br><br></div>
+				We are located only 20 miles south of Chicago, a city that loves baseball so much that it has two professional teams. If you haven't experienced Chicago baseball, have you really experienced Chicago? Complete the experience by visiting a local company that loves baseball as much as the region it calls home.
+				<br>
+				<br>
+				<div class="about-sub">Because this is our dream.<br><br></div>
+				Creating bats in a plant where visitors can see that process and customize their bat has been a dream many years in the making. Patience and prayer have made that dream a reality. We know the quality of each bat will show our commitment to creating a fine and lasting product.
+				<br>
+				<br>
+				<div class="about-sub">Because baseball brings people together.<br><br></div>
+				For generations families, neighbors, friend and foe alike, have gathered in their local park, in Grandma's back yard, and in professional stadiums to enjoy a game of baseball...
+				<br>
+				<br>
+				<div class="about-sub">Come visit us and see for yourself!</div>
+			</div>
+		</div>
+
+		<div id="mobile-about" class="mobile-about">
 			<div class="about-title">WHY ANOTHER BAT COMPANY?</div>
 			<p>
 			<div class="about-body">
--- script.js
+++ script.js
@@ -1,28 +1,57 @@
 $(document).ready(function(){
-	$slideClicked = false;
+	$slideVisible = false;
 
    $(".hamburger").click(function() {
-
-   		
-   		if($slideClicked == false) {
-   			$(".slide-menu").animate({
-				    height: "300"
-				  }, 500, function() {
-				    // Animation complete.
-				});
-
-   			$slideClicked = true;
-   		}
-   		else{
-   			$(".slide-menu").animate({
-				    height: "0"
-				  }, 500, function() {
-				    // Animation complete.
-				});
-
-   			$slideClicked = false;
-   		}
+      $slideVisible = $('.slide-menu').is(':visible');
+      if($slideVisible == false){
+         $(".slide-menu").slideDown("slow");
+      }
+      else {
+         $(".slide-menu").slideUp("slow");
+      }
    });
+
+
+   $("#shop").click(function() {
+      $("#sec2").scrollView();
+   });
+
+   $("#news").click(function() {
+      $("#sec3").scrollView();
+   });
+   $("#about").click(function() {
+      $("#sec5").scrollView();
+   });
+   $("#contact").click(function() {
+      $("#sec4").scrollView();
+   });
+   $("#home").click(function() {
+      $("#sec1").scrollView();
+   });
+
+   $("#mshop").click(function() {
+      $("#sec2").scrollViewM();
+      $(".slide-menu").slideUp("fast");
+   });
+
+   $("#mnews").click(function() {
+      $("#sec3").scrollViewM();
+      $(".slide-menu").slideUp("fast");
+   });
+   $("#mabout").click(function() {
+      $("#sec5").scrollViewM();
+      $(".slide-menu").slideUp("fast");
+   });
+   $("#mcontact").click(function() {
+      $("#sec4").scrollViewM();
+      $(".slide-menu").slideUp("fast");
+   });
+   $("#mhome").click(function() {
+      $("#sec1").scrollViewM();
+      $(".slide-menu").slideUp("fast");
+   });
+
+   
 
    $("#cartShow").click(function() {
    		
@@ -36,3 +65,19 @@ $(document).ready(function(){
 
    
 }); 
+
+$.fn.scrollView = function () {
+  return this.each(function () {
+    $('html, body').animate({
+      scrollTop: $(this).offset().top - 125
+    }, 1000);
+  });
+}
+
+$.fn.scrollViewM = function () {
+  return this.each(function () {
+    $('html, body').animate({
+      scrollTop: $(this).offset().top - 75
+    }, 1000);
+  });
+}
