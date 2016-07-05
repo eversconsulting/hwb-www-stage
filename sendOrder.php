8688182686ca204db1f7c4ef054cd76b6795f39d
Brandon Evers <brandonevers@CONTRACTOR-MAC1357.local>
2016-07-05 15:44:16 -0500

    Fix

--- sendOrder.php
+++ sendOrder.php
@@ -38,7 +38,7 @@ foreach($json as $order_bat){
 //SMTP needs accurate times, and the PHP time zone MUST be set
 //This should be done in your php.ini, but this is how to do it if you don't have access to that
 date_default_timezone_set('Etc/UTC');
-require 'PHPMailer/PHPMailerAutoload.php';
+require 'PHPMailerAutoload.php';
 //Create a new PHPMailer instance
 $mail = new PHPMailer;
 //Tell PHPMailer to use SMTP
